2688d958dd1b1916df5031269d8d34fc529f67b8
KKKJYjy <himgaso@gmail.com>
2024-11-09 19:50:27 +0900

    학습 1

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -3,8 +3,11 @@
 using namespace std;
 
 int main() {
+	int a = 0x00112233;
 
-	cout << "hello world!";
+	printf("0x%08X\n", a);
+	printf("%d\n", 10 - 5);
+	printf("%d\n", 10 - (~5 + 1));
 
 	return 0;
 }
